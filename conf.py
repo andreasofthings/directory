86107c6bff0cc4bf84bfc1479bf5a5030b11a783
Andreas Neumeier <andreas@neumeier.org>
2020-06-04 19:59:18 +0200

    fix import

--- conf.py
+++ conf.py
@@ -10,9 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
 # sys.path.insert(0, os.path.abspath('.'))
+sys.path.append(os.path.abspath("./ext"))
 
 
 # -- Project information -----------------------------------------------------
@@ -30,7 +31,6 @@ release = '2020-05-13'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-sys.path.append(os.path.abspath("./ext"))
 extensions = [
 'todo'
 ]
